18dd20c38b0813fa07e25142354aad84233a3619
mister8luch <luisresidentevil18@gmail.com>
2025-10-19 23:28:35 -0300

    Revert "ah"
    This reverts commit f765c2075018816d9dfbbbd474cb94a2e54579e7.

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(8o_c5!6xc&nmk4!ln_#(f6fyki*9wlqv@uf&w62hts6ylgu59
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition

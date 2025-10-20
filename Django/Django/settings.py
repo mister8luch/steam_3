3478fa1f02055f6bbe702b6bc2022d35d51d007a
mister8luch <luisresidentevil18@gmail.com>
2025-10-20 00:15:53 -0300

    Update settings.py

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(8o_c5!6xc&nmk4!ln_#(f6fyki*9wlqv@uf&w62hts6ylgu59
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['steam-3.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['steam-3.onrender.com']
 
 
 # Application definition
